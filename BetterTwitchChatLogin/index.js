6d16dbcb96b5e0e7153fcf047888f158dd62aa60
Strencher <46447572+Strencher@users.noreply.github.com>
2020-10-17 18:54:23 +0200

    whooops

--- BetterTwitchChatLogin/index.js
+++ BetterTwitchChatLogin/index.js
@@ -11,12 +11,14 @@ if('access_token' in window.params) {
             'Accept': 'application/vnd.twitchtv.v5+json'
         }
     }).then(res => res.json()).then(res => {
+        console.log(res)
         const [user] = res.data;
         if(!user) return;
         var el = document.querySelector('#tokenInput');
         el.setAttribute('out', JSON.stringify({
             token: window.params.access_token,
-            avatar: user.profile_image_url
+            avatar: user.profile_image_url,
+            name: user.login
         }));
         el.addEventListener('click', () => {
             navigator.clipboard.writeText(el.getAttribute('out'));
