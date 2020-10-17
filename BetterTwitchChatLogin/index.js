4fec6368deec6e9a5f4aef88817d34ac01b0a468
Strencher <46447572+Strencher@users.noreply.github.com>
2020-10-17 14:41:35 +0200

    oauth for BTC

---
+++ BetterTwitchChatLogin/index.js
@@ -0,0 +1,26 @@
+var clientId = 'td3Z5QnN1IWa6N3azVzdjd3a4lXelVGeuZHMmZWe';
+function decodeString(string = "", method = true) {
+	return (method ? btoa : atob)(string.split("").reverse().join("")).split("").reverse().join("");
+}
+if('access_token' in window.params) {
+    fetch('https://api.twitch.tv/helix/users', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${window.params.access_token}`,
+            'Client-ID': decodeString(clientId, false),
+            'Accept': 'application/vnd.twitchtv.v5+json'
+        }
+    }).then(res => res.json()).then(res => {
+        const [user] = res.data;
+        if(!user) return;
+        var el = document.querySelector('#tokenInput');
+        el.setAttribute('out', JSON.stringify({
+            token: window.params.access_token,
+            avatar: user.profile_image_url
+        }));
+        el.addEventListener('click', () => {
+            navigator.clipboard.writeText(el.getAttribute('out'));
+        });
+        el.innerText = 'Copy Login Info';
+    });
+}
