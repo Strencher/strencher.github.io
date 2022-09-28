345400b66a23528cb2a8ee0eefb59fa9ebae7d1a
Strencher <46447572+Strencher@users.noreply.github.com>
2022-09-28 17:22:58 +0200

    Update utils.js

--- download/utils.js
+++ download/utils.js
@@ -4,7 +4,7 @@ export async function startDownload(src, filename) {
   try {
     const res = await fetch(src);
     const data = await res.text();
-    $(`<a href="data:text/javascript;charset=utf-8,${encodeURIComponent(data)}" download="${filename}">`).click();
+    $(`<a href="data:text/${filename.endsWith('.js') ? "javascript" : "css"};charset=utf-8,${encodeURIComponent(data)}" download="${filename}">`).click();
   } catch (error) {
     $('#text').textContent = 'Sorry, an error has occurred.';
     console.error(error);
